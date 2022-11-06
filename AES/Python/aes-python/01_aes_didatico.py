41cad21ef37a815c59c907fea51378e2169b6211
Igor Almeida <igorervalia3@gmail.com>
2022-11-06 17:13:23 -0300

    Iniciando trabalho

---
+++ AES/Python/aes-python/01_aes_didatico.py
@@ -0,0 +1,10 @@
+"""
+Essa é uma implementação "from scratch" da cifra AES-128
+Implementação feita separando as etapas de criptografia add_round_key, sub_bytes, shift_rows e mix_columns.
+Computacionalmente ineficiente, porém didático.
+"""
+
+
+
+
+
